3136db9696eb57093f0fc69222e24c65b395c71f
crisca807 <crisca807@gmail.com>
2024-03-13 12:13:06 -0500

    se agregaron archivos para el desarrollo de req res de la api atravez de metodo http

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 7000;
 
-app.get('/', (req, res) => {
-res.send("hello world");
-});
+app.use("/api/v1/workouts" , v1WorkoutRouter);
+
 
 app.listen(PORT, () => { console.log(`server listening on port ${PORT}`); });
